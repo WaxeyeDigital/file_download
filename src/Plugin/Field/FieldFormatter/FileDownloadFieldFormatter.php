40ec8eb2982c37c24c6e2089714d4d690ae7ed2a
Kent Parker <kent@passingphase.co.nz>
2017-06-03 09:15:52 +1200

    restore missing plus sign

--- src/Plugin/Field/FieldFormatter/FileDownloadFieldFormatter.php
+++ src/Plugin/Field/FieldFormatter/FileDownloadFieldFormatter.php
@@ -144,7 +144,7 @@ class FileDownloadFieldFormatter extends FileFormatterBase {
 
         // Pass field item attributes to the theme function.
         if (isset($item->_attributes)) {
-          $elements[$delta] = array('#attributes' => array());
+          $elements[$delta] += array('#attributes' => array());
           $elements[$delta]['#attributes'] += $item->_attributes;
           // Unset field item attributes since they have been included in the
           // formatter output and should not be rendered in the field template.
